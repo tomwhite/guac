c30287f37fe761d8a9362cbd597db852d8a66df3
Tom White <tom@cloudera.com>
2016-10-23 09:53:46 +0100

    First version

---
+++ src/main/java/com/tom_e_white/App.java
@@ -0,0 +1,13 @@
+package com.tom_e_white;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
